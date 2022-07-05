Fixed the issue with the wrong code units and also added a (quite convoluted) way of finding the correct .dat file for the three different modes of operation.

main.py
@@ -14,7 +14,6 @@ from scipy.signal import savgol_filter
 from digitize import calculate_gravitational_energy
 import time
 import pandas as pd
-import mpld3
 # Author: Adam Fenton
 
 cwd = os.getcwd()
@@ -23,7 +22,7 @@ cwd = os.getcwd()
 au = plonk.units('au')
 kms = plonk.units('km/s')
 density_to_load = None #
-mean_bins_radial = np.logspace(np.log10(0.01),np.log10(100),75)
+mean_bins_radial = np.logspace(np.log10(0.001),np.log10(50),75)
 mpl_colour_defaults=plt.rcParams['axes.prop_cycle'].by_key()['color'] # MLP default colours
 
 # Initalise the figure and output file which is written to later on
@@ -42,11 +41,11 @@ def calculate_sum(binned_quantity,summed_quantity,bins):
     return stats.binned_statistic(binned_quantity, summed_quantity, 'sum', bins=bins)
 
 def calculate_mean(binned_quantity,mean_quantity):
-    bins = np.logspace(np.log10(0.01),np.log10(100),75)
+    bins = np.logspace(np.log10(0.001),np.log10(50),75)
     return stats.binned_statistic(binned_quantity, mean_quantity, 'mean', bins=bins)
 
 def calculate_number_in_bin(binned_quantity,mean_quantity,width):
-    bins=np.logspace(np.log10(0.01),np.log10(width),75)
+    bins=np.logspace(np.log10(0.001),np.log10(width),75)
     return stats.binned_statistic(binned_quantity, mean_quantity, 'count', bins=bins)
 
 def calculate_thermal_energy(subSnap):
@@ -55,7 +54,7 @@ def calculate_thermal_energy(subSnap):
     U *= ((subSnap['my_temp'].magnitude>2000)*(1/1.2)+(subSnap['my_temp'].magnitude<2000)*(1/2.381))
     return U
 
-def prepare_snapshots(snapshot,density_to_load,clump_number):
+def prepare_snapshots(snapshot,density_to_load,clump_number,clump_data_file,density_flag):
     ''' Load full snapshot as plonk object and initialise subsnap centred on clump.
         Also apply filter to exclude dead and accreted particles with `accreted_mask`
         and load units
@@ -77,23 +76,35 @@ def prepare_snapshots(snapshot,density_to_load,clump_number):
     id = np.where(snap['density']== max_elem)
     clump_centre = snap['position'][id]
     clump_dir = "clump"+clump_number
-    clump_location_data = pd.read_csv('%s/%s/%s.dat' % (cwd,clump_dir,clump_number),names=["rho", "x", "y", "z",
-                                                                                           "vx", "vy", "vz"])
-    index = clump_location_data['rho'].sub(density_to_load).abs().idxmin()
 
-    row_of_interest = clump_location_data.iloc[[index]]
-    clump_centre = row_of_interest.values[0][1:4]
+    clump_location_data = pd.read_csv(clump_data_file,names=["rho", "x", "y", "z",
+                                                             "vx", "vy", "vz"])
 
-    clump_velocity = row_of_interest.values[0][4:]
-    print(clump_velocity)
-    x,y,z = clump_centre[0],clump_centre[1],clump_centre[2]
+
+    if flag == 1:
+        index = clump_location_data['rho'].sub(density_to_load).abs().idxmin()
+        row_of_interest = clump_location_data.iloc[[index]]
+        clump_centre = row_of_interest.values[0][1:4]
+        velocity_conversion = 2.978E6/1e5
+        clump_velocity = row_of_interest.values[0][4:] * velocity_conversion * kms
+        x,y,z = clump_centre[0],clump_centre[1],clump_centre[2]
+
+    if flag == 0:
+        snapshot_density = 10**(-(int(snapshot.split(".")[3].split("0")[1])))
+        index = clump_location_data['rho'].sub(snapshot_density).abs().idxmin()
+        row_of_interest = clump_location_data.iloc[[index]]
+        clump_centre = row_of_interest.values[0][1:4]
+        velocity_conversion = 2.978E6/1e5
+        clump_velocity = row_of_interest.values[0][4:] * velocity_conversion * kms
+        x,y,z = clump_centre[0],clump_centre[1],clump_centre[2]
+
 
     # clump_velocity = snap['velocity'][id]
     accreted_mask = snap['smoothing_length'] > 0
     snap_active = snap[accreted_mask]
-    subSnap=plonk.analysis.filters.sphere(snap=snap_active,radius = (100*au),center=clump_centre *au)
+    subSnap=plonk.analysis.filters.sphere(snap=snap_active,radius = (50*au),center=clump_centre *au)
 
-    subSnap_rotvel=plonk.analysis.filters.cylinder(snap=snap_active,radius = (100*au),height=(5*au),center=clump_centre * au)
+    subSnap_rotvel=plonk.analysis.filters.cylinder(snap=snap_active,radius = (50*au),height=(0.1*au),center=clump_centre * au)
 
     subSnap.set_units(position='au', density='g/cm^3',smoothing_length='au',velocity='km/s')
 
@@ -104,20 +115,30 @@ try:
     print('Running script in',sys.argv[1], 'mode')
 
     if sys.argv[1] == 'clump':
+        flag = 0
         print("Loading all clump files from current directory")
         check = input("This should only be used if there is one clump present, proceed? [y/n] ")
         complete_file_list = glob.glob("run*")
+        clump_data_file = complete_file_list[0].split(".")[1]+".dat"
+
 
     elif sys.argv[1] == 'density' and len(sys.argv) == 2:
+        flag = 1
         print('No density provided, using a default value of 1E-3 g/cm')
         density_to_load = 1E-3
         pattern = str(int(np.abs(np.log10(density_to_load)) * 10)).zfill(3)
         complete_file_list = glob.glob("**/*%s.h5" % pattern)
+        clump_data_file = complete_file_list[0].split("/")[0]+"/"+complete_file_list[0].split("/")[0].split('p')[1]+".dat"
+        print(clump_data_file)
 
     elif sys.argv[1] == 'density'  and len(sys.argv) == 3:
+        flag = 1
         density_to_load = float(sys.argv[2])
         pattern = str(int(np.abs(np.log10(density_to_load)) * 10)).zfill(3)
         complete_file_list = glob.glob("**/*%s.h5" % pattern)
+        clump_data_file = complete_file_list[0].split("/")[0]+"/"+complete_file_list[0].split("/")[0].split('p')[1]+".dat"
+        print(clump_data_file)
+
 
 except IndexError:
     print("Plotting mode not provided...exiting")
@@ -152,11 +173,11 @@ for file in tqdm(complete_file_list):
 
     line_colour = mpl_colour_defaults[index]
 
-    prepared_snapshots = prepare_snapshots(file,density_to_load,clump_number)
+    prepared_snapshots = prepare_snapshots(file,density_to_load,clump_number,clump_data_file,flag)
 
     ORIGIN = prepared_snapshots[2]# The position of the clump centre
     x,y,z = ORIGIN[0],ORIGIN[1],ORIGIN[2]
-    vel_ORIGIN = prepared_snapshots[3]*kms  # The velocity of the clump centre
+    vel_ORIGIN = prepared_snapshots[3]  # The velocity of the clump centre
 
     print("from main",vel_ORIGIN)
     # vx,vy,vz = ORIGIN[0]*kms,ORIGIN[1]*kms,ORIGIN[2]*kms
@@ -171,7 +192,7 @@ for file in tqdm(complete_file_list):
     radius_clump = np.sqrt((x)**2 + (y)**2 + (z)**2)
 
 
-    count = calculate_number_in_bin(r_clump_centred,subSnap['m'],100)
+    count = calculate_number_in_bin(r_clump_centred,subSnap['m'],50)
     mass_in_bin = np.cumsum(count[0]) * subSnap['mass'][0].to('jupiter_mass')
     mid_plane_radius = plonk.analysis.particles.mid_plane_radius(subSnap,ORIGIN,ignore_accreted=True)
     rotational_velocity_radial = plonk.analysis.particles.rotational_velocity(subSnap,vel_ORIGIN,ignore_accreted=True)
@@ -289,7 +310,7 @@ for file in tqdm(complete_file_list):
     for i in range(0,3):
         for j in range(0,2):
             f_radial_axs[i,j].set_xscale('log')
-            f_radial_axs[i,j].set_xlim(1E-2,100)
+            f_radial_axs[i,j].set_xlim(1E-3,50)
             f_radial_axs[i,j].set_xlabel('R (AU)',fontsize=18)
             f_radial_axs[i,j].tick_params(axis="x", labelsize=12)
             f_radial_axs[i,j].tick_params(axis="y", labelsize=12)
@@ -299,7 +320,7 @@ for file in tqdm(complete_file_list):
             f_radial_axs[i,j].set_yscale('log')
 
     figure_indexes = [(0,0),(0,1),(1,0),(1,1),(2,0),(2,1)]
-    figure_ylimits = [(1E-13,1E-1),(10,8000),(0,3),(0,1),(0.1,40),(1E-5,10000)]
+    figure_ylimits = [(1E-13,1E-1),(10,8000),(0,7),(0,7),(0.1,40),(1E-5,10000)]
     figure_ylabels = ['Density (g/cm^3)','Temperature (K)','Rotational Velocity (km/s)',
                       'Infall Velocity (km/s)','Mass [Jupiter Masses]','Energy ratio']
 
@@ -315,10 +336,12 @@ for file in tqdm(complete_file_list):
 
     f_radial_axs[1,0].plot(averaged_rotational_velocity[1][1:],averaged_rotational_velocity[0],c = line_colour,linestyle="--",linewidth = 1)
     f_radial_axs[1,0].plot(binned_r_clump_with_nans,average_rotational_with_nans,c = line_colour)
-    f_radial_axs[1,0].scatter(r_clump_centred_midplane_rotvel,rotational_velocity_radial_cyl ,s=0.05)
+    # f_radial_axs[1,0].scatter(r_clump_centred_midplane_rotvel,rotational_velocity_radial_cyl ,s=0.05)
 
     f_radial_axs[1,1].plot(averaged_infall_radial[1][1:],smoothed_infall,c = line_colour,linestyle="--",linewidth = 1)
     f_radial_axs[1,1].plot(binned_r_clump_with_nans,smoothed_infall_nans ,c = line_colour)
+    # f_radial_axs[1,1].scatter(r_clump_centred,infall_velocity_radial ,s=0.05)
+
     f_radial_axs[1,1].plot(x_smooth,bspl_y ,c = 'green',alpha=0.3)
     f_radial_axs[1,1].plot(x_smooth[peaks],bspl_y[peaks],'+',c='red')
 
@@ -417,9 +440,5 @@ for file in tqdm(complete_file_list):
                        rhocritID))
 
 fig_radial.savefig("%s/clump_profiles.png" % cwd,dpi = 500)
-html_str = mpld3.fig_to_html(fig_radial)
-Html_file= open("index.html","w")
-Html_file.write(html_str)
-Html_file.close()
 fig_ang_mom.savefig("%s/specific_angular_momentum.png" % cwd,dpi = 500)
 fig_test.savefig("%s/infall.png" % cwd,dpi = 500)