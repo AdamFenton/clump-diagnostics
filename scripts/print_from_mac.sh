36562fca200636d279b9715deb77a9bae1881cad
AdamFenton <adamfenton96@live.co.uk>
2022-07-27 17:37:21 +0100

    Added a script to print a file using the file explorer on my mac - it isn't really related to the clump diagnostics but I had nowhere else to put it

---
+++ scripts/print_from_mac.sh
@@ -0,0 +1,21 @@
+
+selectedFile="$(osascript -l JavaScript -e 'a=Application.currentApplication();a.includeStandardAdditions=true; a.chooseFile({withPrompt:"Choose a file to print:"}).toString()')"
+selectedFileBasename=$(basename "${selectedFile}")
+printCommand="lpr -P LE302-Colour ~/Downloads/$selectedFileBasename"
+
+echo $selectedFile
+function send_file() {
+  scp -q $selectedFile afenton@stargate:~/Downloads
+}
+function print_file() {
+  ssh -q afenton@stargate $printCommand
+}
+function delete_file() {
+  ssh -q afenton@stargate "rm ~/Downloads/$selectedFileBasename" 2> /dev/null
+}
+
+
+send_file
+echo "File sent to server..."
+print_file
+echo "Printing started..."
