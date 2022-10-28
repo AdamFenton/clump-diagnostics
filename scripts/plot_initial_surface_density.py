805861a052eb92b889f96b648eedb904bb67042e
AdamFenton <adamfenton96@live.co.uk>
2022-10-28 16:55:59 +0100

    Added two small scripts: 1) plot the surface density profile to show the effect of the smoothing term. 2) plot the effect of the self gravity on the velocity of the gas in the disc

---
+++ scripts/plot_initial_surface_density.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+R = np.linspace(10,300,100)
+
+
+def sigma_wo_softening(R):
+    return 1.53E3 * (R/10)**(-1.5)
+def sigma_w_softening(R):
+    return 1.53E3 * (R/10)**(-1.5)*(1-np.sqrt(10/R))
+
+plt.plot(R,sigma_w_softening(R),c='k',linestyle='dashed')
+plt.plot(R,sigma_wo_softening(R),c='k',linestyle='solid')
+plt.yscale('log')
+
+plt.xlabel("R [AU]")
+plt.ylabel("Σ g$\,cm^{-3}$")
+plt.show()
